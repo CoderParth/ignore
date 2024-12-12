d7c3e702587e5aa659a7c6370403b1afbf0bb132
CoderParth <ghantigalli@gmail.com>
2024-12-12 20:33:10 +1100

    feature/remove-duplicates-from-gitignore-file

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Example: `ignore rust`
 
 This will create a .gitignore file for a rust project. 
 
-Incase, you already have a .gitignore file, recommended patterns will be appended 
-to your existing .gitignore file. 
+Incase, you already have a .gitignore file, recommended patterns will be appended, 
+and duplicates will not be added to your existing .gitignore file. 
 
 See `ignore --all` to see the list of supported projects.
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"log"
@@ -13,8 +14,8 @@ Type 'ignore <project-type>' to create a .gitignore file for your project.
 Example: 'ignore rust'
 This will create a .gitignore file for a rust project. 
 
-Incase, you already have a .gitignore file, recommended patterns will be appended 
-to your existing .gitignore file. 
+Incase, you already have a .gitignore file, recommended patterns will be appended, 
+and duplicates will not be added to your existing .gitignore file. 
 
 See 'ignore --all' to see the list of supported projects.
 `
@@ -123,6 +124,25 @@ List of supported projects:
 -> zigo
 `
 
+func create(projectType string) {
+	template := openTemplate(projectType)
+	defer template.Close()
+	isANewFile := createIfNotExist("./.gitignore")
+	ignoreFile := openInAppendMode("./.gitignore")
+	defer ignoreFile.Close()
+	copy(template, ignoreFile)
+	if !isANewFile {
+		fs := fileScanner(".gitignore")
+		arr := createArr(fs) // array of lines of the file
+		unqArr := uniq(arr)
+		str := convToStr(unqArr)
+		f := openInOverWriteMode(".gitignore")
+		defer f.Close()
+		writeToFile(f, str)
+	}
+	fmt.Printf(".gitignore has been created for %s \n", projectType)
+}
+
 func openTemplate(file string) *os.File {
 	f := fmt.Sprintf("./templates/%s", file)
 	template, err := os.Open(f)
@@ -135,14 +155,17 @@ func openTemplate(file string) *os.File {
 	return template
 }
 
-func createIfNotExist(f string) {
+func createIfNotExist(f string) bool {
+	isNew := false
 	_, err := os.Stat(f)
 	if os.IsNotExist(err) {
+		isNew = true
 		_, err := os.Create(f)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
+	return isNew
 }
 
 func openInAppendMode(file string) *os.File {
@@ -160,14 +183,67 @@ func copy(temp, ignoreFile *os.File) {
 	}
 }
 
-func create(projectType string) {
-	template := openTemplate(projectType)
-	defer template.Close()
-	createIfNotExist("./.gitignore")
-	ignoreFile := openInAppendMode("./.gitignore")
-	defer ignoreFile.Close()
-	copy(template, ignoreFile)
-	fmt.Printf(".gitignore has been created for %s \n", projectType)
+func openInOverWriteMode(file string) *os.File {
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return f
+}
+
+func writeToFile(f *os.File, s string) {
+	_, err := f.WriteString(s)
+	if err != nil {
+		log.Fatalf("Error writing to file: %s", err)
+	}
+}
+
+func createArr(fs *bufio.Scanner) []string {
+	arr := []string{}
+	for fs.Scan() {
+		arr = append(arr, fs.Text())
+	}
+	return arr
+}
+
+func uniq(a []string) []string {
+	uniq := []string{}
+	n := len(a)
+	for i := 0; i < n; i++ {
+		if a[i] == " " {
+			uniq = append(uniq, a[i])
+			continue
+		}
+		dup := false
+		for j := 0; j < len(uniq); j++ {
+			if uniq[j] == a[i] {
+				dup = true
+				break
+			}
+		}
+		if !dup {
+			uniq = append(uniq, a[i])
+		}
+	}
+	return uniq
+}
+
+func convToStr(arr []string) string {
+	joinedStr := ""
+	for _, l := range arr {
+		joinedStr += l + "\n"
+	}
+	return joinedStr
+}
+
+func fileScanner(file string) *bufio.Scanner {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fs := bufio.NewScanner(f)
+	fs.Split(bufio.ScanLines)
+	return fs
 }
 
 func main() {
